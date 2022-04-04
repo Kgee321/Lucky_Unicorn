0ff51d25e0baadcce66f749427c503f28961d4ef
Kgee321 <geek@middleton.school.nz>
2022-04-04 12:40:51 +1200

    yup

---
+++ 03_instructions_v1.py
@@ -0,0 +1,42 @@
+""" create an instruction function for when the user does not know how to play """
+
+
+# yes/no checker function goes here:
+def yes_no(asking):
+    while True:
+        # Ask user if they have played the game in the past
+        question = input(asking).lower()
+
+        # If yes, calling on game function
+        if question == "y" or question == "yes":
+            question = "Yes"
+            return question
+
+        # If no, calling on instruction function
+        elif question == "n" or question == "no":
+            question = "No"
+            return question
+
+        # If incorrect input, repeat questions
+        else:
+            print("Incorrect input, please enter yes or no (y/n).")
+    return question
+
+
+# function to display instructions
+def instructions():
+    print("*** How to play ***")
+    print()
+    print("The game rules will go here")
+    print()
+    print("Program continues")
+
+
+# main routine goes here:
+played_before = yes_no("Do you know how to play this game (Lucky Unicorn)? Y/N ")
+
+if played_before == "No":
+    instructions()
+else:
+    print("Program continues")
+
--- 04_how_much_v2.py
+++ 04_how_much_v2.py
@@ -1,29 +1,30 @@
 """Component 2 -- How much version 2
-Editing version 1
+Editing version 1 -- making it more effective and more simple
 Written by Katelyn Gee
 31/03/2022"""
 
 
-def number_checker(question):
-    error = "Error, try again! Please enter a number between 1 and 10."
-    valid = False
+def number_checker(question, low, high):
+    error = "Error, try again! Please enter a whole number between 1 and 10."
+    amount = 0
 
     # Keeps asking until the correct value is entered
-    while not valid:
+    while True:
         try:
             # asking the question
             amount = int(input(question))
-            if 1 <= amount <= 10:
-                # breaks the loop if correct value
-                valid = True
+            print()
+
+            # checking if the number is between the required amount
+            if low <= amount <= high:
                 return amount
             else:
-                # prints error if not between 1 and 10
                 print(error)
+
         except ValueError:
             # print error if not a valid integer
             print(error)
 
 
 # calls on functions and prints how much money the user is using
-print(f"You number is {number_checker('Enter the amount of money you want to play with between 1 and 10: $')}")
+print(f"You are using ${number_checker('Enter the amount of money you want to play with between 1 and 10: $', 1, 10)}")
